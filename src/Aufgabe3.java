98af97c2b40582f21eabc79b748d8fda0f671202
sith98 <sith98@web.de>
2018-11-15 10:45:55 +0100

    Simplified inOrderMinAvlTrees

--- src/Aufgabe3.java
+++ src/Aufgabe3.java
@@ -11,8 +11,8 @@ public class Aufgabe3 {
         }
         return numMinAvlTrees(height - 1) * numMinAvlTrees(height - 2) * 2;
     }
-    
-    
+
+
     private static List<String> inOrderMinAvlTrees(int height) {
         if (height == 0) {
             return List.of("(n,0,n)");
@@ -22,31 +22,20 @@ public class Aufgabe3 {
         }
         var treesMinus1 = inOrderMinAvlTrees(height - 1);
         var treesMinus2 = inOrderMinAvlTrees(height - 2);
-        
+
         List<String> trees = new ArrayList<>();
         for (String treeMinus1 : treesMinus1) {
             for (String treeMinus2 : treesMinus2) {
-                StringBuilder builder = new StringBuilder();
-                builder.append('(');
-                builder.append(treeMinus1);
-                builder.append(",0,");
-                builder.append(treeMinus2);
-                builder.append(')');
-                trees.add(builder.toString());
-    
-                builder = new StringBuilder();
-                builder.append('(');
-                builder.append(treeMinus2);
-                builder.append(",0,");
-                builder.append(treeMinus1);
-                builder.append(')');
-                trees.add(builder.toString());
+                trees.add("(" + treeMinus1 + ",0," + treeMinus2 + ')');
+                trees.add("(" + treeMinus2 + ",0," + treeMinus1 + ')');
             }
         }
         return trees;
     }
-    
+
     public static void main(String[] args) {
+        System.out.println(numMinAvlTrees(5));
+
         var trees = inOrderMinAvlTrees(3);
         trees.forEach(System.out::println);
     }
